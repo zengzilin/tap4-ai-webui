affebe863782d4d4c66eec59ea47c837e903b657
zengzilin <zengzilin@users.noreply.github.com>
2024-10-07 18:37:49 +0800

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -21,10 +21,10 @@ export const RevalidateOneMonth = 3600 * 24 * 30;
 export const PAGE_SIZE = 20;
 export const InfoPageSize = 12;
 
-export const FORM_PLACEHOLDER = 'https://tap4.ai/';
+export const FORM_PLACEHOLDER = 'https://aisphereguide.com';
 
 export const WEBSITE_EXAMPLE =
-  '<a href="https://tap4.ai/" title="Tap4 AI Tools Directory">Tap4 AI Tools Diresctory</a>';
+  '<a href="https://aisphereguide.com" title="Tap4 AI Tools Directory">Tap4 AI Tools Diresctory</a>';
 
 export const STARTUP_LIST = [
   {
